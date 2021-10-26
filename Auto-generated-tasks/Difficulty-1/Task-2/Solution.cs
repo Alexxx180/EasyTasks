a30a9a1ca3d741cd6fb1e3b3fb94d3362223ab56
Alexxx180 <alexx120702@gmail.com>
2021-10-26 11:21:20 +0300

    Auto | Created difficulties 1-2

--- Auto-generated-tasks/Difficulty-1/Task-2/Solution.cs
+++
@@ -1,9 +0,0 @@
-int i, N = 6;
-string[] Spisok = new string[N];
-for (i = 0; i < N/3; i++) {
-	Spisok[i * 3] = "No";
-	Spisok[i * 3 + 1] = "Buf517";
-	Spisok[i * 3 + 2] = "K776";
-}
-for (i = 0; i < N; i++)
-	Console.WriteLine($"Spisok[{i}]={Spisok[i]}");
--- Auto-generated-tasks/Difficulty-1/Task-8/Solution.php
+++
@@ -1,8 +0,0 @@
-<?
-$Kol = 6;
-$q = 0;
-$A = array_fill(0, $Kol, 0);
-for($q = 0; $q < $Kol; $q++) {
-	$A[$q] = $q * 9p;
-}
-?>
